fbbab86d5fa7b7986736344a0092d939d99dbc71
AnuGangarapu <anusha.g@unifo.in>
2025-11-28 00:11:33 +0530

    resolvedCorsERROR

--- src/index.ts
+++ src/index.ts
@@ -20,10 +20,20 @@ connectDatabase();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://workspace-service-rmi1w9bpj-anushas-projects-15d95fe0.vercel.app/api",  // your React domain
+    "https://workspacebooking.netlify.app/",  // your React domain
   ],
   credentials: true,
 }));
+
+// ✅ Required manual headers
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin as string);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 app.use(express.json());
 
 // Health route
